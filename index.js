866d7ec755073edececd167737ae6ae7ae28d900
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-07 11:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function add(x,y) {return x+y}
 function subtract (x,y) {return x-y}
 function multiply (x,y) {return x*y}
 function divide (x,y) {return x/y}
-function inc(n) {return n++}
-function dec(n) {return n--}
+function inc(n) {return n+=1}
+function dec(n) {return n-=1}
 function makeInt(n) {return parseInt(n,10)}
